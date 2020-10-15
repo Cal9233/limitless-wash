cc34a5f305e8e54aea185685dac032af100da4d5
Cal <calvin.m71@hotmail.com>
2020-10-15 13:36:21 -0400

    components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from "react";
-import Navbar from "./components/Navbar";
 import * as s from "./App.styles";
-import { Home, Services, Contact } from "./pages";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/services" component={Services} />
-        </Switch>
-      </Router>
+      <s.App>
+        <s.Header>Sidebar</s.Header>
+      </s.App>
     </>
   );
 }
